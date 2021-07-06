60b4430615f892554871b1b67dbee1b0b4f2a87b
Miguel L. Rodrigues <miguellukas52@gmail.com>
2021-07-06 13:47:06 -0300

    updates

--- transmission/ImageGrab.py
+++ transmission/ImageGrab.py
@@ -1,17 +1,21 @@
 from threading import Thread
+
 import mss
 import numpy as np
 
 
-class GrabImageThread(Thread):
+class ImageGrabThread(Thread):
     def __init__(self, group=None, target=None, name=None,
-                 args=(), kwargs=None, *, daemon=None):
+                 args=(), kwargs=None, *, daemon=None, monitor=None):
         super().__init__(group, target, name, args, kwargs, daemon=daemon)
+
+        if monitor is None:
+            monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
 
         self._return = None
         self.sct = mss.mss()
 
-        self.monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
+        self.monitor = monitor
 
     def run(self):
         self._return = np.array(self.sct.grab(self.monitor))
@@ -19,3 +23,6 @@ class GrabImageThread(Thread):
     def join(self, timeout=None) -> np.ndarray:
         super().join(timeout)
         return self._return
+
+    def stop(self):
+        self.sct.close()
---
+++ transmission/ImageReceive.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import base64
+import socket
+from threading import Thread
+
+
+class ImageReceiverThread(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs=None, *, daemon=None):
+        super().__init__(group, target, name, args, kwargs, daemon=daemon)
+
+        self.kwargs = kwargs
+
+        self.BUFF_SIZE = 65536
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, self.BUFF_SIZE)
+
+        host_ip = self.kwargs['ip']
+        port = self.kwargs['port']
+
+        self.socket.sendto(b'.', (host_ip, port))
+
+        self._return = None
+
+    def run(self):
+        packet, _ = self.socket.recvfrom(self.BUFF_SIZE)
+
+        data = base64.b64decode(packet, ' /')
+
+        np_data = np.frombuffer(data, dtype=np.uint8)
+        frame = cv2.imdecode(np_data, 1)
+
+        self._return = frame
+
+    def join(self, timeout=None) -> np.ndarray:
+        super().join(timeout)
+        return self._return
+
+    def stop(self):
+        self.socket.close()
+
